07adf66b1c96a082bc4fdd6c2a682295936b05b0
NicolasSequeira11 <nicolassequeira1997@hotmail.com>
2023-09-27 19:41:02 -0300

    travel info tours

--- js/travel-info.js
+++ js/travel-info.js
@@ -22,20 +22,44 @@ function showTravelInfo(array) {
   containerInfo.innerHTML = `
     <div class="col-12 m-auto travel-info">
     
-    <div class="m-auto travel-info__img-container">
-      <img class="col-12 travel-info__img" src="${array.imageBanner}">
-      <h2 class="travel-info__title"><strong>${array.name}</strong></h2>
-    </div>
-
-        <div class="col-12 col-lg-5 m-auto container-product-info">
-            
-            <p class="card-description description-product-info">${array.description}</p>
-            <p class="card-description category-product-info"><strong>Categoría</strong> <br> ${array.category}</p>
-            <p class="card-soldcount soldCount-product-info"><strong>Cantidad de vendidos</strong> <br> ${array.soldCount}</p>
-            <p class="card-cost cost-product-info"><strong>Precio</strong> <br> ${array.currency} ${array.cost}</p>
+      <div class="m-auto travel-info__img-container">
+        <img class="col-12 travel-info__img" src="${array.imageBanner}">
+        <div class="travel-info__title-container">
+          <h2 class="travel-info__title"><strong>${array.name}</strong></h2>
         </div>
+      </div>
+
+      <div class="d-flex container">
+        <div class="col-12 col-lg-8 travel-info__include-container">
+          <p class="travel-info__include-title"><strong>Incluye:</strong></p>
+          <p class="travel-info__include-items-container">${includes(array.includes)}</p>
+          <p class="travel-info__include-title"><strong>Tours:</strong></p>
+          <p class="travel-info__include-items-container">${includes(array.tours)}</p>
+        </div>
+
+        <div class="col-12 col-lg-3 travel-info__info-container">
+          <h3 class="travel-info__info-title">${array.name}</h3>
+          <h5 class="travel-info__info-days">${array.days}</h5>
+          <p class="travel-info__info-date">${array.date}</p>
+          <p class="travel-info__info-description">${array.description}</p>
+          <p class="travel-info__info-price"><strong>${array.price}</strong></p>
+          <p class="travel-info__info-priceType">${array.priceType}</p>
+          <button class="travel-info__info-button">Consultar</button>
+        </div>
+      </div>
+
     </div>
   `;
 }
 
+/* Crear lista */
+function includes(array){
+  let container = "";
+
+  for(let i=0; i<array.length; i++){
+    container += `<li class="travel-info__include-items">`+ array[i] + `</li>`;
+  }
+
+  return container;
+  }
 });
